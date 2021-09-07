e8c7e9bfe626f8fa3e8264f70495b00f4b5f4c5c
Ömer Keskin <omer.keskin.deu@hotmail.com>
2021-09-07 23:05:39 +0300

    Spinner development

--- src/script.js
+++ src/script.js
@@ -2,25 +2,29 @@ import { getTodoItems, addTodoItem } from "./model.js";
 import { state } from "./model.js";
 import todoContainerView from "./view/todoContainerView.js";
 
-const addItem = async function(event){
+const addItem = async function (event) {
   event.preventDefault();
   let todoInput = document.getElementById("todo-input");
-  try { 
-    await addTodoItem(todoInput.value); 
-    todoContainerView.render(state.todoItems);  
+  try {
+    await addTodoItem(todoInput.value);
+    todoContainerView.render(state.todoItems);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
-  todoInput.value = '';
+  todoInput.value = "";
 };
 
+const controlClickTodoContainer = function () {
+  console.log('In controller');
+};
 
-const init = async function(){
-   await getTodoItems();
-   const todoInputForm = document.getElementById("todo-input-form");
-   todoInputForm.addEventListener('submit', addItem);
-   todoContainerView.render(state.todoItems);
+const init = async function () {
+  todoContainerView.addHandlerClickTodoContainer(controlClickTodoContainer);
+  todoContainerView.renderSpinner();
+  await getTodoItems();
+  const todoInputForm = document.getElementById("todo-input-form");
+  todoInputForm.addEventListener("submit", addItem);
+  todoContainerView.render(state.todoItems);
 };
 
 init();
-
--- src/view/todoContainerView.js
+++ src/view/todoContainerView.js
@@ -1,5 +1,6 @@
 import iconCheck from 'url:/assets/icon-check.svg';
 import iconCross from 'url:/assets/icon-cross.svg';
+import icons from 'url:/assets/icons.svg';
 
 class TodoContainerView {
 
@@ -47,11 +48,32 @@ class TodoContainerView {
      `;
   }
 
+  renderSpinner() {
+    const markup = `
+      <div class="spinner">
+          <svg>
+                <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>
+   `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
   render(data){
     this._data = data;
     const markup = this._generateMarkup();
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin',markup);
+  }
+
+  addHandlerClickTodoContainer(handler){
+     this._parentElement.addEventListener('click', function(e){
+        const target = e.target;
+        const checkStatusImg = target.closest('.todo-item').querySelector('img');
+        checkStatusImg.src = iconCross;
+        console.log(checkStatusImg);
+     });
   }
   
 }
